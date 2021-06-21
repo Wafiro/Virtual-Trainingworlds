72be033148f7186df5763be749ff81a6aa1253c4
Johannes Madest <johannesmadest@gmail.com>
2021-06-21 18:35:37 +0200

    first steps for cinemachine

---
+++ Assets/CamereTransitions/CameraMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public PlayableDirector director;
+    public List<TimelineAsset> timelines;
+
+    public void Play()
+    {
+        director.Play();
+    }
+
+
+    public void PlayTimeline(int index)
+    {
+        TimelineAsset selected;
+
+        if (timelines.Count <= index)
+        {
+            selected = timelines[timelines.Count - 1];
+        }
+        else
+        {
+            selected = timelines[index];
+        }
+        
+        
+        director.Play(selected);
+    }
+
+}
