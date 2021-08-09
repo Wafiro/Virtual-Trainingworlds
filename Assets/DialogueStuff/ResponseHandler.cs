73b5d546479085c1077e3a2631df4938e6cd360c
Johannes Madest <johannesmadest@gmail.com>
2021-08-09 16:30:36 +0200

    Kofferraum Szene

--- Assets/DialogueStuff/ResponseHandler.cs
+++ Assets/DialogueStuff/ResponseHandler.cs
@@ -4,6 +4,8 @@ using TMPro;
 using TMPro.Examples;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Playables;
 
 public class ResponseHandler : MonoBehaviour
 {
@@ -12,17 +14,24 @@ public class ResponseHandler : MonoBehaviour
     [SerializeField] private RectTransform responseContainer;
 
     private DialogueUI _dialogueUI;
-    
+
     private List<GameObject> tempResponseButton = new List<GameObject>();
+    public GameObject c;
+    public GameObject panel;
+    public GameObject intro;
+    private CameraTransition _cameraTransition;
+    public GameObject buttonField;
+    private MoveButtons _moveButtons;
 
     private void Start()
     {
         _dialogueUI = GetComponent<DialogueUI>();
+        _cameraTransition = c.GetComponent<CameraTransition>();
+        _moveButtons = buttonField.GetComponent<MoveButtons>();
     }
 
     public void ShowResponses(Response[] responses)
     {
-        //float responseBoxHeight = 0;
 
         foreach (Response response in responses)
         {
@@ -34,11 +43,11 @@ public class ResponseHandler : MonoBehaviour
             
             tempResponseButton.Add(responseButton);
 
-           //responseBoxHeight += responseButtonTemplate.sizeDelta.y;
+          
         }
         
-        //responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, responseBoxHeight);
         responseBox.gameObject.SetActive(true);
+        //buttonField.gameObject.SetActive(true);
     }
 
     private void OnPickedResponse(Response response)
@@ -51,8 +60,71 @@ public class ResponseHandler : MonoBehaviour
            Destroy(button);
        }
        tempResponseButton.Clear();
+
+       if (response.id == 0)
+       {
+           _dialogueUI.ShowDialogue(response.DialogueObject);
+       }
+       else
+       {
+           switch (response.id)
+           {
+               case 10:
+                   //Animation fürs Intro
+                   Debug.Log("animation");
+                   StartCoroutine(Intro(response.DialogueObject));
+                   break;
+               case 20:
+                   //Szenenwechsel zum Kofferraum
+                   StartCoroutine(Kofferraum(response.DialogueObject));
+                   break;
+               case 21:
+                   //Animation fürs Warndreieck aufstellen
+                   Debug.Log("animation");
+                   StartCoroutine(Warndreieck(response.DialogueObject));
+                   break;
+               case 31:
+                   //Person aus dem Wagen heben
+                   break;
+               case 41:
+                   //Stabile Seitenlage
+                   break;
+
+           }
+       }
        
-        _dialogueUI.ShowDialogue(response.DialogueObject);
+    }
+
+    public IEnumerator Warndreieck(DialogueObject next)
+    {
+        panel.SetActive(false);
+        _cameraTransition.SwitchState(4);
+        yield return new WaitForSeconds(3);
+        _cameraTransition.dreieck.SetActive(true);
+        yield return new WaitForSeconds(1);
+        _cameraTransition.SwitchState(3);
+        yield return new WaitForSeconds(3);
+        panel.SetActive(true);
+        _dialogueUI.ShowDialogue(next);
+
+    }
+
+    public IEnumerator Intro(DialogueObject next)
+    {
+        panel.SetActive(false);
+        intro.GetComponent<PlayableDirector>().Play();
+        yield return new WaitForSeconds(5);
+        panel.SetActive(true);
+        _dialogueUI.ShowDialogue(next);
+    }
+    
+    public IEnumerator Kofferraum(DialogueObject next)
+    {
+        panel.SetActive(false);
+        _cameraTransition.SwitchState(3);
+        yield return new WaitForSeconds(1);
+        panel.SetActive(true);
+        _dialogueUI.ShowDialogue(next);
     }
 
     void Clicked()
